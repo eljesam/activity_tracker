b3455a2a6734853d468f54cfe4074c427965210f
eljesam <124920260+eljesam@users.noreply.github.com>
2023-10-27 09:57:30 +0100

    change

---
+++ activ_track/src/Running.java
@@ -0,0 +1,12 @@
+public class Running extends Activity {
+    @Override
+    public double getCaloriesBurned() {
+        return 0;
+    }
+
+    @Override
+    public double getEnergySpent() {
+        return 0;
+    }
+
+}
